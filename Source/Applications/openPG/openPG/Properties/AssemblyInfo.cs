31527c74bea3d51082f3cdef64b2ae077573eeaf
ritchiecarroll <rcarroll@gridprotectionalliance.org>
2012-02-13 14:17:50 +0000

    openPG: Version change for build openPG_v0.9.31.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openPG/Main;C74939

--- Source/Applications/openPG/openPG/Properties/AssemblyInfo.cs
+++ Source/Applications/openPG/openPG/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c8a353e9-9383-46c5-8c19-8da53d307cde")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("0.9.0.0")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("0.9.31.0")]
+[assembly: AssemblyFileVersion("0.9.31.0")]
--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.5.6.0")]
-[assembly: AssemblyVersion("0.9.0.0")]
+// [assembly: AssemblyVersion("0.9.31.0")]
+[assembly: AssemblyVersion("0.9.31.0")]
