3b6072b0d3e85f099766e9992a09d9d7219275d3
pinalcpatel <pcpatel@tva.gov>
2012-08-08 21:24:04 +0000

    openPG: Version change for build openPG_v1.0.40.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openPG/Main;C79433

--- Source/Applications/openPG/openPG/Properties/AssemblyInfo.cs
+++ Source/Applications/openPG/openPG/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c8a353e9-9383-46c5-8c19-8da53d307cde")]
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.39.0")]
-[assembly: AssemblyFileVersion("1.0.39.0")]
+[assembly: AssemblyVersion("1.0.40.0")]
+[assembly: AssemblyFileVersion("1.0.40.0")]
