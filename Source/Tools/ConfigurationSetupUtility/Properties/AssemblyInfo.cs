b92b7f6289036a788ce54e58684d14e2db77dcc4
pinalcpatel <pcpatel@tva.gov>
2012-09-26 20:22:45 +0000

    openPG: Version change for build openPG_v1.0.46.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openPG/Main;C80028

--- Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
+++ Source/Tools/ConfigurationSetupUtility/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Windows;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.45.0")]
-[assembly: AssemblyVersion("1.0.45.0")]
+// [assembly: AssemblyVersion("1.0.46.0")]
+[assembly: AssemblyVersion("1.0.46.0")]
