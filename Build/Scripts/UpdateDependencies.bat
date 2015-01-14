ad4fb5faa6373c2f4621f9d901625c1e2da41bad
StephenCWills <swills@gridprotectionalliance.org>
2015-01-14 23:17:06 +0000

    Modified UpdateDependencies.bat script to properly call the new db-update.bat script.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openPG/Main;C92763

--- Build/Scripts/UpdateDependencies.bat
+++ Build/Scripts/UpdateDependencies.bat
@@ -92,7 +92,7 @@ MOVE /Y "%sourceschema%\SQLite\*.*" "%targetschema%\SQLite\"
 %replace% /r /v "%targetschema%\*db-update.bat" GSFSchema substationSBG
 SET cwd=%CD%
 CD %targetschema%\SQLite
-ECHO "N" | db-update.bat
+CALL db-update.bat
 CD "%cwd%"
 
 :: ECHO.
