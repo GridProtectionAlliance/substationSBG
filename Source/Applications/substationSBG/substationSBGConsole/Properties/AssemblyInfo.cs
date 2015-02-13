95f67fc3bc6d7fef7dfe9f27451d4e8861f0997d
gsfbuildbot <support@gridprotectionalliance.org>
2015-02-13 08:37:51 +0000

    substationSBG: Version change for build substationSBG_v0.6.58.0.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/openPG/Main;C94078

--- Source/Applications/substationSBG/substationSBGConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/substationSBG/substationSBGConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("0.6.57.0")]
+[assembly: AssemblyVersion("0.6.58.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
