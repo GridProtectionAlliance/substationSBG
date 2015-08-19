59c91a77235766cef449be9a7cd25981e462a8f0
StephenCWills <swills@gridprotectionalliance.org>
2015-08-19 16:43:15 -0400

    Updated automated build scripts to work with Git.

--- Build/Scripts/UpdateDependencies.bat
+++ Build/Scripts/UpdateDependencies.bat
@@ -25,54 +25,40 @@
 
 @ECHO OFF
 
-SET vs="%VS110COMNTOOLS%\..\IDE\devenv.com"
-SET tfs="%VS110COMNTOOLS%\..\IDE\tf.exe"
+SET pwd="%CD%"
+SET gwd="%LOCALAPPDATA%\Temp\substationSBG"
+SET git="%PROGRAMFILES(X86)%\Git\cmd\git.exe"
 SET replace="\\GPAWEB\NightlyBuilds\Tools\ReplaceInFiles\ReplaceInFiles.exe"
-SET source1="\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Libraries\*.*"
-SET target1="..\..\Source\Dependencies\GSF"
-SET sourceschema=..\..\Source\Dependencies\GSF\Data
-SET targetschema=..\..\Source\Data
-SET solution="..\..\Source\substationSBG.sln"
-SET sourcetools=..\..\Source\Applications\substationSBG\substationSBGSetup\
-SET frameworktools=\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Tools\
-SET /p checkin=Check-in updates (Y or N)? 
+
+SET remote="git@github.com:GridProtectionAlliance/substationSBG.git"
+SET source="\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Libraries\*.*"
+SET target="Source\Dependencies\GSF"
+SET sourceschema=Source\Dependencies\GSF\Data
+SET targetschema=Source\Data
+SET sourcetools=\\GPAWEB\NightlyBuilds\GridSolutionsFramework\Beta\Tools\
+SET targettools=Source\Applications\substationSBG\substationSBGSetup\
+
+ECHO.
+ECHO Entering working directory...
+IF EXIST %gwd% RMDIR /S /Q %gwd%
+MKDIR %gwd%
+CD %gwd%
 
 ECHO.
 ECHO Getting latest version...
-%tfs% get %target1% /version:T /force /recursive /noprompt
-%tfs% get "%sourcetools%ConfigCrypter.exe" /version:T /force /recursive /noprompt
-%tfs% get "%sourcetools%ConfigurationEditor.exe" /version:T /force /recursive /noprompt
-%tfs% get "%sourcetools%DataMigrationUtility.exe" /force /recursive /noprompt
-%tfs% get "%sourcetools%HistorianPlaybackUtility.exe" /force /recursive /noprompt
-%tfs% get "%sourcetools%HistorianView.exe" /force /recursive /noprompt
-%tfs% get "%sourcetools%StatHistorianReportGenerator.exe" /version:T /force /recursive /noprompt
-%tfs% get "%sourcetools%NoInetFixUtil.exe" /version:T /force /recursive /noprompt
-%tfs% get "%sourcetools%DNP3ConfigGenerator.exe" /version:T /force /recursive /noprompt
-
-ECHO.
-ECHO Checking out dependencies...
-%tfs% checkout %target1% /recursive /noprompt
-%tfs% checkout "%sourcetools%ConfigCrypter.exe" /noprompt
-%tfs% checkout "%sourcetools%ConfigurationEditor.exe" /noprompt
-%tfs% checkout "%sourcetools%DataMigrationUtility.exe" /noprompt
-%tfs% checkout "%sourcetools%HistorianPlaybackUtility.exe" /noprompt
-%tfs% checkout "%sourcetools%HistorianView.exe" /noprompt
-%tfs% checkout "%sourcetools%StatHistorianReportGenerator.exe" /noprompt
-%tfs% checkout "%sourcetools%NoInetFixUtil.exe" /noprompt
-%tfs% checkout "%sourcetools%DNP3ConfigGenerator.exe" /noprompt
-%tfs% checkout "%targetschema%" /recursive /noprompt
+%git% clone %remote% .
 
 ECHO.
 ECHO Updating dependencies...
-XCOPY %source1% %target1% /Y /E
-XCOPY "%frameworktools%ConfigCrypter\ConfigCrypter.exe" "%sourcetools%ConfigCrypter.exe" /Y
-XCOPY "%frameworktools%ConfigEditor\ConfigEditor.exe" "%sourcetools%ConfigurationEditor.exe" /Y
-XCOPY "%frameworktools%DataMigrationUtility\DataMigrationUtility.exe" "%sourcetools%DataMigrationUtility.exe" /Y
-XCOPY "%frameworktools%HistorianPlaybackUtility\HistorianPlaybackUtility.exe" "%sourcetools%HistorianPlaybackUtility.exe" /Y
-XCOPY "%frameworktools%HistorianView\HistorianView.exe" "%sourcetools%HistorianView.exe" /Y
-XCOPY "%frameworktools%StatHistorianReportGenerator\StatHistorianReportGenerator.exe" "%sourcetools%StatHistorianReportGenerator.exe" /Y
-XCOPY "%frameworktools%NoInetFixUtil\NoInetFixUtil.exe" "%sourcetools%NoInetFixUtil.exe" /Y
-XCOPY "%frameworktools%DNP3ConfigGenerator\DNP3ConfigGenerator.exe" "%sourcetools%DNP3ConfigGenerator.exe" /Y
+XCOPY %source% %target% /Y /E
+XCOPY "%sourcetools%ConfigCrypter\ConfigCrypter.exe" "%targettools%ConfigCrypter.exe" /Y
+XCOPY "%sourcetools%ConfigEditor\ConfigEditor.exe" "%targettools%ConfigurationEditor.exe" /Y
+XCOPY "%sourcetools%DataMigrationUtility\DataMigrationUtility.exe" "%targettools%DataMigrationUtility.exe" /Y
+XCOPY "%sourcetools%HistorianPlaybackUtility\HistorianPlaybackUtility.exe" "%targettools%HistorianPlaybackUtility.exe" /Y
+XCOPY "%sourcetools%HistorianView\HistorianView.exe" "%targettools%HistorianView.exe" /Y
+XCOPY "%sourcetools%StatHistorianReportGenerator\StatHistorianReportGenerator.exe" "%targettools%StatHistorianReportGenerator.exe" /Y
+XCOPY "%sourcetools%NoInetFixUtil\NoInetFixUtil.exe" "%targettools%NoInetFixUtil.exe" /Y
+XCOPY "%sourcetools%DNP3ConfigGenerator\DNP3ConfigGenerator.exe" "%targettools%DNP3ConfigGenerator.exe" /Y
 
 ECHO.
 ECHO Updating database schema defintions...
@@ -91,32 +77,19 @@ MOVE /Y "%sourceschema%\SQLite\*.*" "%targetschema%\SQLite\"
 %replace% /r /v "%targetschema%\*SampleDataSet.sql" 6165 6180
 %replace% /r /v "%targetschema%\*SampleDataSet.sql" "e7a5235d-cb6f-4864-a96e-a8686f36e599" "8a8d1856-ebc8-4238-848a-8084b7dd9541"
 %replace% /r /v "%targetschema%\*db-update.bat" GSFSchema substationSBG
-SET cwd=%CD%
 CD %targetschema%\SQLite
 CALL db-update.bat
-CD "%cwd%"
+CD %gwd%
 
-:: ECHO.
-:: ECHO Building solution...
-:: %vs% %solution% /Build "Release|Any CPU"
-
-IF /I "%checkin%" == "Y" GOTO Checkin
-GOTO Finalize
-
-:Checkin
 ECHO.
-ECHO Checking in dependencies...
-%tfs% checkin %target1% /noprompt /recursive /comment:"Updated grid solutions framework dependencies."
-%tfs% checkin "%sourcetools%ConfigCrypter.exe" /noprompt /comment:"Updated grid solutions framework tool: ConfigCrypter."
-%tfs% checkin "%sourcetools%ConfigurationEditor.exe" /noprompt /comment:"Updated grid solutions framework tool: ConfigurationEditor."
-%tfs% checkin "%sourcetools%DataMigrationUtility.exe" /noprompt /comment:"Synchrophasor-VS2012: Updated grid solutions framework tool: DataMigrationUtility."
-%tfs% checkin "%sourcetools%HistorianPlaybackUtility.exe" /noprompt /comment:"Synchrophasor-VS2012: Updated openHistorian playback / export tool: HistorianPlaybackUtility."
-%tfs% checkin "%sourcetools%HistorianView.exe" /noprompt /comment:"Synchrophasor-VS2012: Updated openHistorian trending tool: HistorianView."
-%tfs% checkin "%sourcetools%StatHistorianReportGenerator.exe" /noprompt /comment:"Updated grid solutions framework tool: StatHistorianReportGenerator."
-%tfs% checkin "%sourcetools%NoInetFixUtil.exe" /noprompt /comment:"Updated grid solutions framework tool: NoInetFixUtil."
-%tfs% checkin "%sourcetools%DNP3ConfigGenerator.exe" /noprompt /comment:"Synchrophasor-VS2012: Updated grid solutions framework tool: DNP3ConfigGenerator."
-%tfs% checkin "%targetschema%" /noprompt /recursive /comment:"Updated database schema definitions from GSF source."
+ECHO Committing updates to local repository...
+%git% add .
+%git% commit -m "Updated GSF dependencies."
 
-:Finalize
+ECHO.
+ECHO Pushing changes to remote repository...
+%git% push
+CD %pwd%
+
 ECHO.
 ECHO Update complete
