51b42d4045061ab0386e8df8e55286a4f1e82864
StephenCWills <swills@gridprotectionalliance.org>
2016-05-20 16:53:42 -0400

    Changed reference to openPDC.sql to substationSBG.sql in the Configuration Setup Utility.

--- Source/Tools/ConfigurationSetupUtility/Screens/SetupInProgressScreen.xaml.cs
+++ Source/Tools/ConfigurationSetupUtility/Screens/SetupInProgressScreen.xaml.cs
@@ -842,7 +842,7 @@ namespace ConfigurationSetupUtility.Screens
                         int progress = 0;
 
                         // Determine which scripts need to be run.
-                        scriptNames.Add("openPDC.sql");
+                        scriptNames.Add("substationSBG.sql");
                         if (initialDataScript)
                         {
                             scriptNames.Add("InitialDataSet.sql");
