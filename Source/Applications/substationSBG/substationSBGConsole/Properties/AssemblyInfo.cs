6ecc5ff9ccd6d170ba3f417fc7810c70b5617bef
gsfbuildbot <support@gridprotectionalliance.org>
2016-09-21 01:01:45 -0400

    substationSBG: Version change for build v1.0.330.0-master.

--- Source/Applications/substationSBG/substationSBGConsole/Properties/AssemblyInfo.cs
+++ Source/Applications/substationSBG/substationSBGConsole/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.InteropServices;
 
 // Assembly identity attributes.
-[assembly: AssemblyVersion("1.0.329.0")]
+[assembly: AssemblyVersion("1.0.330.0")]
 
 // Informational attributes.
 [assembly: AssemblyCompany("Grid Protection Alliance")]
